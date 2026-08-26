ee064cf5768911df8f904bf89fc0410c1b29efb5
DavidMaksic <david.maksic88@gmail.com>
2026-08-26 19:11:26 +0200

    Refactor test

--- e2e/post.test.ts
+++ e2e/post.test.ts
@@ -21,9 +21,6 @@ test.describe('Post creation', () => {
 
       // Fill describe input and select a category
       await page.getByLabel('description').fill('My first E2E test post');
-      await expect(
-         page.locator('#category option:nth-child(2)'),
-      ).toBeAttached();
       await page.locator('#category').selectOption({ index: 1 });
 
       // Add a tag
